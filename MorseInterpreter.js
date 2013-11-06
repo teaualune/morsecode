0a7b8549cad0db9700c0c41d8b76fd93650f43aa
teaualune <teaualune@gmail.com>
2013-11-06 13:29:35 +0800

    build a simple version for practice

--- MorseInterpreter.js
+++ MorseInterpreter.js
@@ -1,75 +1,64 @@
-(function () {
-    var MI = function (morseTable, source) {
+var MorseInterpreter = function (morseTable, source) {
 
-            var toCode = function (b) {
-                    var code;
-                    if (b.length === 1) {
-                        code = '.';
-                    } else if (b.length === 3) {
-                        code = '-';
-                    }
-                    return code;
-                },
-
-                toLetter = function (c) {
-                    return morseTable[c.join('')];
-                },
-
-                toWord = function (w) {
-                    return w.join('');
-                },
-
-                buffer = [],
-                letter = [],
-                word = [],
-                output = [],
-                sc = 0,
-
-                divider = function (_sc) {
-                    if (_sc === 1) { // construct a code
-                        letter.push(toCode(buffer));
-                        buffer = [];
-                    } else if (_sc === 3) { // construct a letter
-                        word.push(toLetter(letter));
-                        letter = [];
-                    } else if (_sc === 6) { // construct a word
-                        if (output.length > 0) {
-                            output.push(' ');
-                        }
-                        output.push(toWord(word));
-                        word = [];
-                    }
-                },
-
-                c = null;
-
-            if ('string' === typeof source) {
-                source = source.split('');
-            }
-
-            while (c = source.shift()) {
-                if (c === '*') { // space
-                    sc = sc + 1;
-                } else if (c === '=') { // signal
-                    buffer.push(c);
-                    sc = 0;
+        var toCode = function (b) {
+                var code;
+                if (b.length === 1) {
+                    code = '.';
+                } else if (b.length === 3) {
+                    code = '-';
                 }
-                divider(sc);
+                return code;
+            },
+
+            toLetter = function (c) {
+                return morseTable[c.join('')];
+            },
+
+            toWord = function (w) {
+                return w.join('');
+            },
+
+            buffer = [],
+            letter = [],
+            word = [],
+            output = [],
+            sc = 0,
+
+            divider = function (_sc) {
+                if (_sc === 1) { // construct a code
+                    letter.push(toCode(buffer));
+                    buffer = [];
+                } else if (_sc === 3) { // construct a letter
+                    word.push(toLetter(letter));
+                    letter = [];
+                } else if (_sc === 6) { // construct a word
+                    if (output.length > 0) {
+                        output.push(' ');
+                    }
+                    output.push(toWord(word));
+                    word = [];
+                }
+            },
+
+            c = null;
+
+        if ('string' === typeof source) {
+            source = source.split('');
+        }
+
+        while (c = source.shift()) {
+            if (c === '*') { // space
+                sc = sc + 1;
+            } else if (c === '=') { // signal
+                buffer.push(c);
+                sc = 0;
             }
+            divider(sc);
+        }
 
-            // finalization
-            divider(1);
-            divider(3);
-            divider(6);
-            return output.join('');
-        };
-
-    if (module && module.exports) {
-        module.exports = MI;
-    } else if (window) {
-        window.MorseInterpreter = MI;
-    } else {
-        MorseInterpreter = MI;
-    }
-
-}());
+        // finalization
+        divider(1);
+        divider(3);
+        divider(6);
+        return output.join('');
+    };
--- MorseTranslator.js
+++ MorseTranslator.js
@@ -1,38 +1,27 @@
-(function () {
-    var MT = function (morseTable, source) {
-            var output = [],
-                c = null,
-                buffer = null,
-                b;
+var MorseTranslator = function (morseTable, source) {
+        var output = [],
+            c = null,
+            buffer = null,
+            b;
 
-            if ('string' === typeof source) {
-                source = source.split('');
-            }
-            while (c = source.shift()) {
-                if (c === ' ') {
+        if ('string' === typeof source) {
+            source = source.split('');
+        }
+        while (c = source.shift()) {
+            if (c === ' ') {
+                output.push('***');
+            } else {
+                if (output.length > 0) {
                     output.push('***');
-                } else {
-                    if (output.length > 0) {
-                        output.push('***');
-                    }
-                    c = c.toUpperCase();
-                    buffer = morseTable[c];
-                    buffer = buffer.split('').join('*').split('');
-                    for (b = 0; b < buffer.length; b = b + 1) {
-                        buffer[b] = buffer[b].replace('-', '===').replace('.', '=');
-                    }
-                    output.push(buffer.join(''));
                 }
+                c = c.toUpperCase();
+                buffer = morseTable[c];
+                buffer = buffer.split('').join('*').split('');
+                for (b = 0; b < buffer.length; b = b + 1) {
+                    buffer[b] = buffer[b].replace('-', '===').replace('.', '=');
+                }
+                output.push(buffer.join(''));
             }
-            return output.join('');
-        };
-
-    if (module && module.exports) {
-        module.exports = MT;
-    } else if (window) {
-        window.MorseTranslator = MT;
-    } else {
-        MorseTranslator = MT;
-    }
-
-}());
+        }
+        return output.join('');
+    };
